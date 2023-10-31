3ac99e0d1f52769be29e2d0e630df832957b0e8c
NickPrivate <dragonwolf478@gmail.com>
2023-10-30 21:55:02 -0700

    Added functionality for DownloadFile, ThreadHandler, and DownloadStats.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,40 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <chrono>
+#include <ctime>
 
 
 #define num_threads 10
+#define concurrent_downloads 6
+
+struct Download_Stats {
+    std::string fileName;
+    float downloadTime;
+    bool downloadStatus;
+};
+
+void DownloadFile (const std::string& URL, const std::string& fileName){
+
+}
+
+void ThreadHandler(sem_t* semaphore, const std::string& URL, const std::string& fileName, Download_Stats& downloadStats){
+
+    sem_wait(semaphore);
+
+    auto start = std::chrono::system_clock::now();
+    DownloadFile(URL,fileName);
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+
+}
+
+
+
 
 int main(){
 
