ceb39f1a539a463a6d7effc4bf17cca27e1164d1
NickPrivate <dragonwolf478@gmail.com>
2023-11-01 20:06:02 -0700

    download file function complete

--- main.cpp
+++ main.cpp
@@ -25,9 +25,43 @@ size_t PreDownload(void* contents, size_t size, size_t nmemb, std::ofstream* fil
 }
 
 
-void DownloadFile(const std::string& URL, const std::string& fileName){
-    
+void DownloadFile(const std::string& URL, const std::string& fileName, Download_Stats& downloadStats) {
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        // Initialize libcurl with the URL to download
+        curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
 
+        // Open a file for writing the downloaded data
+        std::ofstream file(fileName, std::ios::binary);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file for writing: " << fileName << std::endl;
+            downloadStats.downloadStatus = false;
+            return;
+        }
+
+        // Set the write callback to save downloaded data to the file
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, PreDownload);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+
+        // Perform the download
+        auto start = std::chrono::system_clock::now();
+        CURLcode res = curl_easy_perform(curl);
+        auto end = std::chrono::system_clock::now();
+
+        if (res != CURLE_OK) {
+            std::cerr << "Download failed: " << curl_easy_strerror(res) << std::endl;
+            downloadStats.downloadStatus = false;
+        } else {
+            downloadStats.downloadStatus = true;
+            downloadStats.fileName = fileName;
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            downloadStats.downloadTime = elapsed_seconds.count();
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+        file.close();
+    }
 }
 
 void ThreadHandler(sem_t* semaphore, const std::string& URL, const std::string& fileName, Download_Stats& downloadStats){
